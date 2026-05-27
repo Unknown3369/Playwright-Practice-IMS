f01ac3c616a9cde1383d98c392718f9ad6591a2e
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2026-05-27 15:37:41 +0545

    need to debug the code where every test is being displayed as passed test

---
+++ Pages/Transactions/debit_note.py
@@ -0,0 +1,84 @@
+
+
+from playwright.sync_api import Page, expect
+import time
+
+
+class DebitNote:
+   def __init__(self, page: Page):
+      self.page = page
+
+      self.transactions = "//span[contains(text(), 'Transactions')]"
+      self.pur_transaction = "text='Purchase Transaction'"
+      self.debit_note = "//span[normalize-space()='Debit Note (Purchase Return)']"
+      self.ref_no = "#invoiceNO"
+      self.return_mode = "#paymentTerms"
+      self.supplier = "#customerselectid"
+      self.select_supplier = "//div[normalize-space()='11 QA Vendor']"
+      self.item_name = "#barcodeField"
+      self.quantity = "#quantityBarcode"
+      self.save_button = "//button[normalize-space(text())='SAVE [End]']"
+
+   def enter_debit_note(self):
+
+      self.page.get_by_title("Transactions").first.click()
+      self.page.get_by_title("Purchase Transaction").nth(1).click()
+      self.page.get_by_role("link", name="Debit Note (Purchase Return)").click()
+
+   def debit_note_entry(self, enter_ref_no: str):
+
+      # Enter Reference Number
+      ref_no = self.page.locator(self.ref_no)
+      ref_no.click()
+      ref_no.fill(enter_ref_no)
+
+      print(f"Reference No '{enter_ref_no}' entered successfully!")
+
+      # Select Return Mode
+      self.page.select_option(self.return_mode, label="Cash")
+      print("Return mode 'Cash' selected successfully!")
+
+      # Select Supplier
+      supplier = self.page.locator(self.supplier)
+      supplier.press("Enter")
+
+      select_supplier = self.page.locator(self.select_supplier)
+      select_supplier.dblclick()
+      print("Supplier selected successfully!")
+
+   def debit_note_test(self, item_code: str, enter_quantity: int):
+
+      # Enter Item Code
+      item_name = self.page.locator(self.item_name)
+      item_name.clear()
+      item_name.fill(item_code)
+      self.page.wait_for_timeout(2000)
+      item_name.press("Enter")
+      self.page.wait_for_timeout(2000)
+      print("Item name entered successfully!")
+
+      # Enter Quantity
+      quantity = self.page.locator(self.quantity)
+      quantity.clear()
+      quantity.fill(str(enter_quantity))
+      quantity.press("Enter")
+      print("Quantity entered successfully!")
+      self.page.wait_for_timeout(2000)
+
+   def save_button_click(self):
+
+      # Click Save Button
+      save_button = self.page.locator(self.save_button)
+      expect(save_button).to_be_enabled()
+      save_button.click()
+      print("Save button clicked successfully!")
+      # Handle Alert
+      dialog_message = None
+
+      def handle_dialog(dialog):
+            nonlocal dialog_message
+            dialog_message = dialog.message
+            print("Alert says:", dialog_message)
+            dialog.accept()
+
+      self.page.once("dialog", handle_dialog)
---
+++ Tests/Transactions/test_debit_note.py
@@ -0,0 +1,46 @@
+# tests/test_debit_note.py
+
+import csv
+import random
+import pytest
+from playwright.sync_api import sync_playwright
+from Pages.Login import login
+from Pages.Transactions.debit_note import DebitNote
+
+
+def read_products_from_csv(file_path):
+   products = []
+
+   with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+      reader = csv.DictReader(file)
+      for row in reader:
+            products.append(row)
+      return products
+   
+@pytest.fixture(scope="function")
+def page():
+   with sync_playwright() as p:
+      browser = p.chromium.launch(headless=False)
+      page = browser.new_page()
+      yield page
+      browser.close()
+
+
+def test_debit_note(page):
+
+   login_page = login(page)
+   debit_note = DebitNote(page)
+   page.wait_for_timeout(15000)
+   login_page.perform_login("Testuser", "Test@1234")
+   products = read_products_from_csv("product_details.csv")
+   random_ref_no = "REF_NO" + str(random.randint(10000, 99999))
+   debit_note.enter_debit_note()
+   debit_note.debit_note_entry(str(random_ref_no))
+
+   for product in products:
+      item_code = product["Item Code"]
+      random_quantity = random.randint(1, 10)
+      debit_note.debit_note_test(item_code, random_quantity)
+      page.wait_for_timeout(1000)
+
+   debit_note.save_button_click()
