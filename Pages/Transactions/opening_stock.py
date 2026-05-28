3a41f398117c03c912ce2362a7f13980ca57ef2d
Sagan Krishna Tamrakar <87718989+Unknown3369@users.noreply.github.com>
2026-05-28 17:24:15 +0545

    completed Opening stock and fixed the debit note

---
+++ Pages/Transactions/opening_stock.py
@@ -0,0 +1,64 @@
+
+import csv
+from random import random
+
+from playwright.sync_api import Page
+import time
+
+class OpeningStockPage:
+
+   def __init__(self, page: Page):
+      self.page = page
+
+   def enter_opening_stock(self):
+      self.page.get_by_title("Transactions").first.click()
+      self.page.get_by_title("Inventory Movement").nth(1).click()
+      self.page.get_by_role("link", name="Opening Stock").click()
+
+   def opening_stock_test(
+      self,item_code: str,enter_quantity: int,row_index: int):
+
+      item_field = self.page.locator("#barcodeField")
+
+      item_field.wait_for(
+         state="visible",
+         timeout=30000
+      )
+
+      item_field.clear()
+      item_field.fill(item_code)
+      self.page.wait_for_timeout(1000)
+      item_field.press("Enter")
+      item_field.press("Enter")
+      print("Item name entered successfully!")
+      self.page.wait_for_timeout(1000)
+
+      quantity = self.page.locator("#quantityBarcode")
+      quantity.wait_for(
+            state="visible",
+            timeout=30000
+      )
+      quantity.clear()
+      quantity.fill(str(enter_quantity))
+      quantity.press("Enter")
+
+      print(f"Quantity entered successfully! -> {enter_quantity}")
+      self.page.wait_for_timeout(1000)
+
+   def save_button_click(self):
+
+      save_btn = self.page.locator("//button[normalize-space()='SAVE [End]']")
+      save_btn.wait_for(state="visible", timeout=30000)
+      save_btn.click()
+      self.page.wait_for_timeout(3000)
+      success_message = self.page.locator("//p[contains(text(),'has been saved successfully')]")
+
+      success_message.wait_for(
+         state="visible",
+         timeout=30000
+      )
+      message_text = success_message.text_content()
+
+      print(f"SUCCESS MESSAGE: {message_text}")
+
+      assert "has been saved successfully" in message_text
---
+++ Tests/Transactions/test_03_opening_stock.py
@@ -0,0 +1,62 @@
+import csv
+import random
+import pytest
+
+from playwright.sync_api import sync_playwright
+
+from Pages.Login import login
+from Pages.Transactions.opening_stock import OpeningStockPage
+
+
+def read_products_from_csv(file_path):
+   products = []
+   with open(
+      file_path,
+      mode="r",
+      newline="",
+      encoding="utf-8"
+   ) as file:
+      reader = csv.DictReader(file)
+
+      for row in reader:
+         products.append(row)
+   return products
+
+
+@pytest.fixture(scope="function")
+def page():
+   with sync_playwright() as p:
+      browser = p.chromium.launch(headless=True)
+      page = browser.new_page()
+      yield page
+      browser.close()
+
+
+def test_opening_stock_entry(page):
+
+   login_page = login(page)
+   login_page.perform_login(
+      "Testuser",
+      "Test@1234"
+   )
+
+   print("Logged into IMS")
+   page.wait_for_load_state("networkidle")
+   opening_stock = OpeningStockPage(page)
+   products = read_products_from_csv(
+      "product_details.csv"
+   )
+
+   opening_stock.enter_opening_stock()
+   for index, product in enumerate(products):
+      item_code = product["Item Code"]
+      random_quantity = random.randint(100, 200)
+      opening_stock.opening_stock_test(
+         item_code,
+         random_quantity,
+         index
+      )
+      page.wait_for_timeout(1000)
+
+   opening_stock.save_button_click()
+   print("Opening Stock entry created successfully.")
